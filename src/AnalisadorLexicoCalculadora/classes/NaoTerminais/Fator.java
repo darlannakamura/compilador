a44f48ee197d316ce0ad87d526b167fcf9c6cacc
darlannakamura <darlannakamura@hotmail.com>
2018-11-12 21:21:06 -0200

    Classes Nao terminais

---
+++ src/AnalisadorLexicoCalculadora/classes/NaoTerminais/Fator.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AnalisadorLexicoCalculadora.classes.NaoTerminais;
+
+import AnalisadorLexicoCalculadora.classes.NaoTerminais.Expressao_Interface;
+
+/**
+ *
+ * @author darla
+ */
+public abstract class Fator implements Expressao_Interface{
+
+}
